edcddbd5bdbaeccefa56782570a57744c29c962d
mtshotshob911 <mtshotshob@gmail.com>
2017-05-15 14:40:59 +0200

    Update runlengthencode.js

--- runlengthencode.js
+++ runlengthencode.js
@@ -1,6 +1,7 @@
 process.argv.forEach((val, index) => {
 
   if(index === 2){
+    
    var inputVal = val.split(',');
    var stripped_input = inputVal.join('');
    var input_as_string = stripped_input.toString().substring(1,stripped_input.length-1);	
@@ -12,16 +13,6 @@ process.argv.forEach((val, index) => {
 
 function encodeInput(input) {
     var encoding = [];
-    var prev, count, i;
-    for (count = 1, prev = input[0], i = 1; i < input.length; i++) {
-        if (input[i] != prev) {
-            encoding.push([count, prev]);
-            count = 1;
-            prev = input[i];
-        }
-        else 
-            count ++;
-    }
-    encoding.push([count, prev]);
+    input.match(/(.)\1*/g).forEach(function(substr){ encoding.push([substr.length, substr[0]]) });
     return encoding;
 }
