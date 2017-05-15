d8fb31f212983cfaa2caedb826c3d8ef6b8a6008
mtshotshob911 <mtshotshob@gmail.com>
2017-05-15 14:38:34 +0200

    Update runlengthencode.js

--- runlengthencode.js
+++ runlengthencode.js
@@ -1,13 +1,27 @@
-process.argv.forEach(function (val, index) {
-	if(index === 2){
-	  var inputval = val;
-	  console.log(flatten(inputval));
-	}
-	
+process.argv.forEach((val, index) => {
+
+  if(index === 2){
+   var inputVal = val.split(',');
+   var stripped_input = inputVal.join('');
+   var input_as_string = stripped_input.toString().substring(1,stripped_input.length-1);	
+   console.log(input_as_string);
+   console.log(encodeInput(input_as_string));
+  }
+
 });
 
 function encodeInput(input) {
     var encoding = [];
-    input.match(/(.)\1*/g).forEach(function(substr){ encoding.push([substr.length, substr[0]]) });
+    var prev, count, i;
+    for (count = 1, prev = input[0], i = 1; i < input.length; i++) {
+        if (input[i] != prev) {
+            encoding.push([count, prev]);
+            count = 1;
+            prev = input[i];
+        }
+        else 
+            count ++;
+    }
+    encoding.push([count, prev]);
     return encoding;
 }
