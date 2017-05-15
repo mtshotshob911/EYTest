0a9957a06c99432482b41cf3252176c70efdb5bf
mtshotshob911 <mtshotshob@gmail.com>
2017-05-15 10:20:10 +0200

    updated

--- palindrome.js
+++ palindrome.js
@@ -1,18 +1,20 @@
 process.argv.forEach((val, index) => {
-  
+
   if(index === 2){
      // condition satisfied for input
-     var number = val.split(",");
+     var numbers_as_input = val.split(',');
 
      // checking for integers anything other that integers log error
-     var IsNumericInput = number.filter(function(i){
+     var IsNumericInput = numbers_as_input.filter(function(i){
                                     return isNaN(i);
                                     }).length > 0;
 
 if(IsNumericInput){
   //convert to String for evaliation
-  var input = number.join('')[0];
-  console.log(isPalindrome(input));
+  var stripped_input = numbers_as_input.join('');
+  var input_as_string = stripped_input.toString().substring(1,stripped_input.length-1);
+
+  console.log(isPalindrome(input_as_string));
 }else{
   console.log('Invalid data type supplied. Please enter list of integers e.g [1,4,6,7]');
 }
@@ -20,7 +22,8 @@ if(IsNumericInput){
 
 });
 
-function isPalindrome(s,i) {
+function isPalindrome(input_string,counter) {
  //better perfomance with non branching tail recursion
- return (i=i||0)<0||i>=s.length>>1||s[i]==s[s.length-1-i]&&isPalindrome(s,++i);
+ return (counter=counter||0)<0||counter>=input_string.length>>1||input_string[counter]==input_string[input_string.length-1-counter]&&isPalindrome(input_string,++counter);
 }
+
