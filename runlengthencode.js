e96f26d6d7f039207cc7a90bfef490949acc21e9
mtshotshob911 <mtshotshob@gmail.com>
2017-05-15 10:57:42 +0200

    updated

--- runlengthencode.js
+++ runlengthencode.js
@@ -1,14 +1,7 @@
 process.argv.forEach((val, index) => {
 
   if(index === 2){
-     // condition satisfied for input
-    //  var input = val.split(",");
-    //  console.log(input.join(''));
-    //  if(Array.isArray(input.join(''))){
-    //    console.log(input);
-    //  }
-
-    console.log(encodeInput(val));
+      console.log(encodeInput(val));
   }
 
 });
